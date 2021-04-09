8c316e4d9a09164ccf0b4c60c5ca2632e063e4c9
shojibhasan <shojibhasan15@gmail.com>
2021-04-09 22:52:32 +0600

    password chacker

--- Python Official Doc practrice/password_Checker.py
+++ Python Official Doc practrice/password_Checker.py
@@ -1,25 +1,30 @@
+
 # valid password filters:
 # a) at least 8 character
 # b) start with a number
 # c) last 2 character must be capital
 
 import re
+password = input('Type The Password: ')
+password_list = list(password)
+password_list_length= len(password)
+flag = 0
+while True:
+    if (password_list_length<8):
+        break
+    elif not re.search("[0-9]", password_list[0]):
+        flag = -1
+        break
 
-passowrd= input("Input your password: ")
-x = True
-while x:  
-    if (len(passowrd)<8):
+    elif not re.search("[A-Z]", password_list[password_list_length-1]):
+        flag = -1
         break
-    elif not re.search("[0-9]",passowrd):
-        break
-    elif not re.search("[A-Z]",passowrd):
-        break
-    elif re.search("\s",passowrd):
+    elif not re.search("[A-Z]", password_list[password_list_length-2]):
+        flag = -1
         break
     else:
+        flag = 0
         print("Valid Password")
-        x=False
         break
-
-if x:
+if flag ==-1:
     print("Not a Valid Password")
