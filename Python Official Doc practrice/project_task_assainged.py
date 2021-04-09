7eb927d75a7e9f0442a60317bfc31e8d794480ce
shojibhasan <shojibhasan15@gmail.com>
2021-04-09 21:27:57 +0600

    project task

---
+++ Python Official Doc practrice/project_task_assainged.py
@@ -0,0 +1,10 @@
+# As a project manager you have to assign tasks to developers. Suppose you have a
+# project of 25 modules. 1 developer can complete a module in 3 days and cost 500 USD
+# in a day to find out the cost and time of completing the project with 5 developers.
+
+total_module =25
+one_developer_cost = 500*3
+total_cost = one_developer_cost*5
+print("Total Cost: ",total_cost)
+total_time = 25//3
+print("Total Time Need: ",total_time)
