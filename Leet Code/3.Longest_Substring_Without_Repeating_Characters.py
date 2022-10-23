b9ab5b2b14a0dc546fa1a1144eacc84436fc6136
mahfujul.hasan <mahfujul.hasan@bjitgroup.com>
2022-10-23 23:22:05 +0600

    leetcode 3

--- Leet Code/3.Longest_Substring_Without_Repeating_Characters.py
+++ Leet Code/3.Longest_Substring_Without_Repeating_Characters.py
@@ -1,5 +1,13 @@
-
-
-a = "wwwpkew"
-a = set(a)
-print(len(a))
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        empty_list = ""
+        
+        for i in s:
+            if i not in empty_list:
+                empty_list = empty_list+i
+        return len(empty_list)
+    
+    
+h = Solution()
+s = "pwwkew"
+print(h.lengthOfLongestSubstring(s))
